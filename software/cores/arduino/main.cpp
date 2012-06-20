dff208780e5ad63d82802389dbb3cd257a06a71a
Bernard Xiong <bernard.xiong@gmail.com>
2012-06-20 22:34:06 +0800

    main function is implemented as a weak function.

--- software/cores/arduino/main.cpp
+++ software/cores/arduino/main.cpp
@@ -1,14 +1,21 @@
 #include <Arduino.h>
 
-int main(void)
-{
-	init();
-
-	setup();
-
-	for (;;) {
-		loop();
-	}
-
-	return 0;
+extern "C"{
+int __main(void);
 }
+
+int __main(void)
+{
+   init();
+
+   setup();
+
+   for (;;) {
+      loop();
+   }
+
+   return 0;
+}
+
+/* mark as a weak main function implementation */
+int main() __attribute__ ((weak,alias("__main")));
