0045ac539eeae16277319719eae8175596a736c1
Rogerz Zhang <rogerz.zhang@gmail.com>
2012-07-16 21:13:12 +0800

    Add batch file for environment setting

---
+++ software/platform/setenv.bat
@@ -0,0 +1,5 @@
+@set path=e:\develop\CodeSourcery\Sourcery G++ Lite\bin;D:\Program Files\SEGGER\JLinkARM_V450j;d:\Python25\Scripts;%PATH%
+@set RTT_ROOT=e:\workspace\rt-thread
+@set RTT_EXEC_PATH=e:\develop\CodeSourcery\Sourcery G++ Lite\bin
+@set RTT_CC=gcc
+@cmd
