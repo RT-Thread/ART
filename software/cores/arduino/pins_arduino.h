77d231b2434a089a91b1cbb32f4addc38b383eca
Rogerz Zhang <rogerz.zhang@gmail.com>
2012-07-17 20:13:53 +0800

    Add pin definition for analog inputs

--- software/cores/arduino/pins_arduino.h
+++ software/cores/arduino/pins_arduino.h
@@ -3,4 +3,17 @@
 
 #define F_CPU 10000
 
+static const uint8_t A0 = 0;
+static const uint8_t A1 = 1;
+static const uint8_t A2 = 2;
+static const uint8_t A3 = 3;
+static const uint8_t A4 = 4;
+static const uint8_t A5 = 5;
+static const uint8_t A6 = 6;
+static const uint8_t A7 = 7;
+static const uint8_t A8 = 8;
+static const uint8_t A9 = 9;
+static const uint8_t A10 = 10;
+static const uint8_t A11 = 11;
+
 #endif
