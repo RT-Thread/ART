249a85322a5eb074657a370707fd5ef1a0e2387d
Bernard Xiong <bernard.xiong@gmail.com>
2012-07-22 15:03:32 +0800

    Add cplusplus and serial example.

---
+++ software/examples/cplusplus/cplusplus.cpp
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+#include <rtthread.h>
+
+class CTest
+{
+public:
+	CTest(int a, int b);
+	int a;
+	int b;
+};
+
+CTest::CTest(int a, int b)
+	: a(a), b(b)
+{
+}
+
+CTest t(10, 100);
+
+int main(void)
+{
+	rt_kprintf("t.a = %d, t.b = %d\n", t.a, t.b);
+}
