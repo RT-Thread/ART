72fb170fa49ef68bd00bcd8f2a3d46bed826e119
Rogerz Zhang <rogerz.zhang@gmail.com>
2013-01-19 19:10:10 +0800

    Fix compile error

--- software/platform/wifi_8686/gspi_io.h
+++ software/platform/wifi_8686/gspi_io.h
@@ -6,16 +6,16 @@
 #ifndef _GSPI_IO_H
 #define _GSPI_IO_H
 //typedef char s8;
-typedef unsigned char u8;
+//typedef unsigned char u8;
 
-typedef signed short s16;
-typedef unsigned short u16;
+//typedef signed short s16;
+//typedef unsigned short u16;
 
 //typedef signed long s32;
 //typedef unsigned long u32;
 
-typedef signed long long s64;
-typedef unsigned long long u64;
+//typedef signed long long s64;
+//typedef unsigned long long u64;
 
 #define GPIO_PORTA_DIR_V        (0x1000f004)
 #define GPIO_PORTA_SEL_V    	(0x1000f008)
